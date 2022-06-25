aaec2f98f7a7a5c49c3f5d080e5f3ee2445147b2
Evandro Machado Pereira <evandro.pereira@ufsc.br>
2022-06-25 17:07:25 -0300

    queue system

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 var http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-    cors:{
-        origin:"*"
+    cors: {
+        origin: "*"
     }
 });
 
@@ -20,17 +20,29 @@ app.post('/sendData', function (req, res) {
     //send data to sockets.
     console.log("teste");
     io.sockets.emit('parar', { message: "Hello from server!" })
-    
+
     res.send({});
 });
 
 // When a new connection is requested
+
+var client = null;
+var fila = [];
+
 io.on('connection', function (socket) {
     console.log('User Connected!');
 
-    // Send to the connected user
-    io.emit('event', { message: 'Connected !!!!' });
-    
+    socket.on("isClient", (data) => {
+        if (client == null) {
+            console.log("Sem fila");
+            client = socket;
+        } else {
+            console.log("com fila");
+            fila.push(socket);
+            socket.emit('bloqueado', fila.length);
+        }
+    });
+
     // On each "status", run this function
     socket.on("seguirFrente", (data) => {
         console.log("recebeu frente");
@@ -53,6 +65,23 @@ io.on('connection', function (socket) {
         console.log("recebeu parar");
         io.emit('parar');
     });
+
+
+    socket.on("disconnect", () => {
+        
+
+        if(client.id == socket.id){
+            if (fila.length > 0) {
+                client = fila.shift();
+                client.emit("liberado");
+            }else{
+                client = null;
+            }
+        }else{
+            fila = fila.filter(item => item.id !== socket.id);
+        }      
+    });
+
 });
 
 // Listen to port 3000
